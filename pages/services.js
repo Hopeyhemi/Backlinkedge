b4396bb144d5bbe39ec27394f0c31cfa9634d49d
starr <starr@DESKTOP-P30J3OU>
2023-09-18 14:24:45 +0100

    first commit

--- pages/services.js
+++ pages/services.js
@@ -1,21 +1,11 @@
-// import Services from '@layouts/Services'
-// import React from 'react'
-// import { getListPage } from "../lib/contentParser";
-// const services = ({frontmatter}) => {
-//     const {feature } = frontmatter;
-//   return (
-//     <div>
-//         <Services feature={feature}/>
-//     </div>
-//   )
-// }
-// export const getStaticProps = async () => {
-//     const homePage = await getListPage("content/_index.md");
-//     const { frontmatter } = homePage;
-//     return {
-//       props: {
-//         frontmatter,
-//       },
-//     };
-//   };
-// export default services;
+import Services from '@layouts/Services'
+import React from 'react'
+const services = () => {
+  return (
+    <div>
+        <h2>Services</h2>
+    </div>
+  )
+}
+
+export default services;
